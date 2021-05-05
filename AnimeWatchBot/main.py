2bd5155ac473cec78b0171dbe043df3609ab9ef1
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-05-06 00:51:01 +0530

    Create main.py

---
+++ AnimeWatchBot/main.py
@@ -0,0 +1,22 @@
+import glob
+import logging
+from pathlib import Path
+from AnimeWatchBot.utils import load_plugins
+from AnimeWatchBot import GogoAnime
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+path = "AnimeWatchBot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("Successfully")
+print("Enjoy")
+
+if __name__ == "__main__":
+    GogoAnime.run_until_disconnected()
