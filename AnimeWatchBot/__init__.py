10e2bb2a2d8b299eddce87f7d28760c12cee2f99
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-05-06 14:36:11 +0530

    Update __init__.py

--- AnimeWatchBot/__init__.py
+++ AnimeWatchBot/__init__.py
@@ -1,7 +1,7 @@
 import os
 import time
 import logging
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -10,4 +10,4 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("TOKEN", None)
 
-GogoAnime = TelegramClient('bot', APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
+GogoAnime = TelegramClient('GogoAnime', APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
